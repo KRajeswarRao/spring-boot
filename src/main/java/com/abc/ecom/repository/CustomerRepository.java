524ee7963624d5cea85f134bb611cdb95aa3be44
KRajeswarRao <83784876+KRajeswarRao@users.noreply.github.com>
2022-04-11 09:54:58 +0530

    Delete src directory

--- src/main/java/com/abc/ecom/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abc.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abc.ecom.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-
-}
